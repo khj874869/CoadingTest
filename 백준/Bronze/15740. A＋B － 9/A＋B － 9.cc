8b670062411101e2f54a070cb2fef0d745e4ba5f
khj874869 <134674147+khj874869@users.noreply.github.com>
2026-04-14 11:24:17 +0900

    [Bronze V] Title: A+B - 9, Time: 0 ms, Memory: 2024 KB, Score: 75 point -BaekjoonHub

--- 백준/Bronze/15740. A＋B － 9/A＋B － 9.cc
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.cc
@@ -1,8 +1,60 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
-int main(){
-    int A,B;
-    cin >> A >>  B;
-    cout << A + B;
+
+using i128 = __int128_t;
+
+i128 strToInt128(const string& s) {
+    i128 num = 0;
+    int sign = 1;
+    int idx = 0;
+
+    if (s[0] == '-') {
+        sign = -1;
+        idx = 1;
+    }
+
+    for (; idx < s.size(); idx++) {
+        num = num * 10 + (s[idx] - '0');
+    }
+
+    return num * sign;
+}
+
+void printInt128(i128 num) {
+    if (num == 0) {
+        cout << 0;
+        return;
+    }
+
+    if (num < 0) {
+        cout << '-';
+        num = -num;
+    }
+
+    string result;
+    while (num > 0) {
+        result += (num % 10) + '0';
+        num /= 10;
+    }
+
+    reverse(result.begin(), result.end());
+    cout << result;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string a, b;
+    cin >> a >> b;
+
+    i128 x = strToInt128(a);
+    i128 y = strToInt128(b);
+
+    i128 sum = x + y;
+    printInt128(sum);
+
     return 0;
 }
